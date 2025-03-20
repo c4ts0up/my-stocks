f2f8c222bddda8ddb94d39eb48d47b436d9a5acf
alvaro <a.baccap@uniandes.edu.co>
2025-03-20 16:40:02 -0500

    Basic fetch implementation

--- backend/fetcher/fetcher.go
+++ backend/fetcher/fetcher.go
@@ -1,19 +1,64 @@
 package fetcher
 
-import "github.com/c4ts0up/my-stocks/backend/models"
+import (
+	"encoding/json"
+	"errors"
+	"github.com/c4ts0up/my-stocks/backend/models"
+	"io"
+	"net/http"
+)
 
 type IFetcherApi interface {
-	FetchStockData(url string) ([]models.StockRating, error)
-	SaveStockData([]models.StockRating) error
+	FetchStockData(url string) ([]models.StockRating, string, error)
+	SaveStockData([]models.StockRatingRaw) error
 }
 
 // BasicStockFetcher implements a basic fetch
 type BasicStockFetcher struct{}
 
-func (s *BasicStockFetcher) FetchStockData(url string) ([]models.StockRating, error) {
-	return []models.StockRating{}, nil
+// FetchStockData pulls data from an API and converts it
+func (s *BasicStockFetcher) FetchStockData(url string) ([]models.StockRating, string, error) {
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, "", errors.New("failed to fetch data")
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, "", errors.New("received invalid response from API")
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, "", errors.New("failed to read response body")
+	}
+
+	// obtains the stock query response
+	var apiResponses models.StockQueryResponse
+	if err := json.Unmarshal(body, &apiResponses); err != nil {
+		return nil, "", errors.New("failed to parse JSON response")
+	}
+
+	// iterates through the stocks
+	var stockRatings []models.StockRating
+	for _, rawStock := range apiResponses.Stocks {
+		stock, err := convertApiResponse(rawStock)
+		if err != nil {
+			return nil, "", err
+		}
+		stockRatings = append(stockRatings, stock)
+	}
+
+	err = resp.Body.Close()
+	if err != nil {
+		return nil, "", err
+	}
+
+	return stockRatings, apiResponses.NextPage, nil
 }
 
-func (s *BasicStockFetcher) SaveStockData([]models.StockRating) error {
+// SaveStockData saves stock data to the database (stub implementation)
+func (s *BasicStockFetcher) SaveStockData(stockList []models.StockRating) error {
+	// TODO: Implement this with GORM later
 	return nil
 }
