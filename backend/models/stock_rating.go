78dbe77aad79fdd5432d4ac0438e4c92b7f39b2f
Álvaro A. Bacca <a.baccap@uniandes.edu.co>
2025-03-20 17:07:29 -0500

    Merge pull request #4
    * Base Fetcher boilerplate code
    
    * Stock Rating model correction
    
    * Basic fetch implementation
    
    * Fetcher implementation + tests

--- backend/models/stock_rating.go
+++ backend/models/stock_rating.go
@@ -7,12 +7,31 @@ import "gorm.io/gorm"
 type StockRating struct {
 	gorm.Model           // Includes the ID uint as entry key
 	Ticker     string    `json:"ticker"`
-	TargetFrom string    `json:"target_from"`
-	TargetTo   string    `json:"target_to"`
+	TargetFrom float64   `json:"target_from"` // TODO: can use uint64 to avoid floating error accumulation
+	TargetTo   float64   `json:"target_to"`
 	Company    string    `json:"company"`
 	Action     string    `json:"action"`
 	Brokerage  string    `json:"brokerage"`
 	RatingFrom string    `json:"rating_from"`
 	RatingTo   string    `json:"rating_to"`
 	Time       time.Time `json:"time"`
+}
+
+// StockRatingRaw matches the raw stock structure in a response
+type StockRatingRaw struct {
+	Ticker     string `json:"ticker"`
+	TargetFrom string `json:"target_from"`
+	TargetTo   string `json:"target_to"`
+	Company    string `json:"company"`
+	Action     string `json:"action"`
+	Brokerage  string `json:"brokerage"`
+	RatingFrom string `json:"rating_from"`
+	RatingTo   string `json:"rating_to"`
+	Time       string `json:"time"`
+}
+
+// StockQueryResponse matches the JSON response when querying stocks
+type StockQueryResponse struct {
+	Stocks   []StockRatingRaw `json:"items"`
+	NextPage string           `json:"next_page"`
 }
