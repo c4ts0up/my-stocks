169cdbbeed7daaba79e01addbc0a708e3d2ddc4f
alvaro <a.baccap@uniandes.edu.co>
2025-03-20 15:35:29 -0500

    Database ID documentation

--- backend/models/stock_rating.go
+++ backend/models/stock_rating.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 
 // StockRating represents a single stock rating update
 type StockRating struct {
-	gorm.Model
+	gorm.Model           // Includes the ID uint as entry key
 	Ticker     string    `json:"ticker"`
 	TargetFrom string    `json:"target_from"`
 	TargetTo   string    `json:"target_to"`
