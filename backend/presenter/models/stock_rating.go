4c533d8ebe45280c0a72722ec246ca2452a0ac3b
alvaro <a.baccap@uniandes.edu.co>
2025-03-21 12:18:57 -0500

    New API models

--- backend/presenter/models/stock_rating.go
+++ backend/presenter/models/stock_rating.go
@@ -1,15 +1,29 @@
 package presenter
 
-// StockRatingResponse matches the structure in a response
-type StockRatingResponse struct {
+// StockBase shows the basic information regarding a stock
+type StockBase struct {
 	Ticker         string `json:"ticker"`
-	TargetFrom     string `json:"target_from"`
-	TargetTo       string `json:"target_to"`
-	Company        string `json:"company"`
-	Action         string `json:"action"`
-	Brokerage      string `json:"brokerage"`
-	RatingFrom     string `json:"rating_from"`
-	RatingTo       string `json:"rating_to"`
-	Time           string `json:"time"`
+	CompanyName    string `json:"company_name"`
+	CurrentPrice   string `json:"current_price"`
 	Recommendation string `json:"recommendation"`
 }
+
+// StockRating represents the information related to a stock rating
+type StockRating struct {
+	TargetFrom string `json:"target_from"`
+	TargetTo   string `json:"target_to"`
+	Action     string `json:"action"`
+	Brokerage  string `json:"brokerage"`
+	RatingFrom string `json:"rating_from"`
+	RatingTo   string `json:"rating_to"`
+	Time       string `json:"time"`
+}
+
+// StockDetail represents the whole information of a stock and its details
+type StockDetail struct {
+	StockBase    StockBase     `json:"stock_base"`
+	StockRatings []StockRating `json:"stock_ratings"`
+}
+
+// StockList gives a base list of all stocks
+type StockList []StockBase
