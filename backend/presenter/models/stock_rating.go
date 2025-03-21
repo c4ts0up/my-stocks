ce557db6e62964e4116c30b4db9b232cd110a542
alvaro <a.baccap@uniandes.edu.co>
2025-03-21 01:11:27 -0500

    Recommendation field added to response

--- backend/presenter/models/stock_rating.go
+++ backend/presenter/models/stock_rating.go
@@ -2,13 +2,14 @@ package presenter
 
 // StockRatingResponse matches the structure in a response
 type StockRatingResponse struct {
-	Ticker     string `json:"ticker"`
-	TargetFrom string `json:"target_from"`
-	TargetTo   string `json:"target_to"`
-	Company    string `json:"company"`
-	Action     string `json:"action"`
-	Brokerage  string `json:"brokerage"`
-	RatingFrom string `json:"rating_from"`
-	RatingTo   string `json:"rating_to"`
-	Time       string `json:"time"`
+	Ticker         string `json:"ticker"`
+	TargetFrom     string `json:"target_from"`
+	TargetTo       string `json:"target_to"`
+	Company        string `json:"company"`
+	Action         string `json:"action"`
+	Brokerage      string `json:"brokerage"`
+	RatingFrom     string `json:"rating_from"`
+	RatingTo       string `json:"rating_to"`
+	Time           string `json:"time"`
+	Recommendation string `json:"recommendation"`
 }
