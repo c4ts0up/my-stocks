d9eb9474abee79bbcbe05a01045bf27d6a2ac46b
alvaro <a.baccap@uniandes.edu.co>
2025-04-09 11:05:09 -0500

    [WIP] Better card tests

--- frontend/src/components/StockDetailsModal.vue
+++ frontend/src/components/StockDetailsModal.vue
@@ -1,18 +1,18 @@
 <template>
   <div v-if="stock" class="fixed inset-0 bg-gray-300 bg-opacity-75 flex items-center justify-center p-4">
     <div class="bg-white p-6 rounded-lg shadow-xl max-w-[800px] w-full border border-gray-300 text-center">
-      <h3 class="mb-1 text-gray-800">{{ stock.stock_base.ticker }}</h3>
-      <h2 class="mb-1 text-2xl font-semibold text-gray-900 title-font">{{ stock.stock_base.company_name }}</h2>
-      <h3 class="mb-4 text-gray-800">{{ stock.stock_base.last_price.toFixed(2) }} $</h3>
+      <h3 class="mb-1 text-gray-800" data-testid="ticker">{{ stock.stock_base.ticker }}</h3>
+      <h2 class="mb-1 text-2xl font-semibold text-gray-900 title-font" data-testid="companyName">{{ stock.stock_base.company_name }}</h2>
+      <h3 class="mb-4 text-gray-800" data-testid="currentPrice">{{ stock.stock_base.last_price.toFixed(2) }} $</h3>
 
-      <span :class="getRecommendationClass(stock.stock_base.recommendation, SIZE.xl)">
+      <span :class="getRecommendationClass(stock.stock_base.recommendation, SIZE.xl)" data-testid="recommendationTag">
         {{ stock.stock_base.recommendation === "N/A" ? "" : stock.stock_base.recommendation }}
       </span>
 
-
+is dat
       <h3 class="mt-10 font-semibold text-gray-900">Ratings:</h3>
       <table class="mt-2 mb-2">
-        <tbody v-for="rating in stock.stock_ratings" :key="rating.time" class="text-base text-gray-700">
+        <tbody v-for="rating in stock.stock_ratings" :key="rating.time" class="text-base text-gray-700" data-testid="ratings">
         <tr>
           <td class="text-left px-2 py-1">{{ formatDate(rating.time) }}</td>
           <td class="text-left px-2 py-1">{{ rating.brokerage }}</td>
